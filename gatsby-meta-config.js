6e240b63719b2348d9ffca87960726ed1d522abb
nueavv <nuguni@kakao.com>
2023-05-01 13:47:21 +0000

    [fix] author

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: `Hello! Cloud`,
   description: `Blog posted about ...`,
-  author: `@nueavv`,
+  author: `[Your Name]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
   siteUrl: `https://nueavv.github.io`, 
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `@nueavv`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
