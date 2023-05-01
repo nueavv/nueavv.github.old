4fdf65f7f0d483e72b4228df5deab9c88a5a7056
nueavv <nuguni@kakao.com>
2023-05-02 01:23:40 +0900

    [update] profile image

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -67,7 +67,7 @@ export const Bio = forwardRef((props, ref) => {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile.png/" }) {
+    avatar: file(absolutePath: { regex: "/profile.jpeg/" }) {
       childImageSharp {
         fixed(width: 72, height: 72) {
           ...GatsbyImageSharpFixed
