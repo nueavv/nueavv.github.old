d132304256e6bd28600a62ece309f595159b4cc6
nueavv <nuguni@kakao.com>
2023-05-03 23:46:01 +0900

    사이즈 재 변경

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -18,7 +18,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(36),
+          maxWidth: rhythm(33),
           padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
         }}
       >
