34b00947a22705ba3be5519488aa6c3ac823f37c
nueavv <nuguni@kakao.com>
2023-06-07 23:23:11 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,12 +148,6 @@ module.exports = {
           {
             resolve: `gatsby-remark-mermaid`,
             options: {
-              launchOptions: {
-                executablePath: 'path/to/chrome/executable'
-              },
-              svgo: {
-                plugins: [{ name: 'removeTitle', active: false }]
-              },
               mermaidOptions: {
                 theme: 'neutral',
                 themeCSS: '.node rect { fill: #fff; }'
